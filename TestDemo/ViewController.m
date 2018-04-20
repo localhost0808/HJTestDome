8771403a80f4bfe76ed31fde8fda38346230fb49
Harry <Localhost0808@gmail.com>
2018-04-20 21:35:03 +0800

    mark

--- TestDemo/ViewController.m
+++ TestDemo/ViewController.m
@@ -22,22 +22,21 @@
     NSString * strencoding = [NSString stringWithCString:[str UTF8String] encoding:NSUTF8StringEncoding];
     
     
-    
+#warning 下面UTF8-encoding编码 新旧是否形同
     
     
     
 //    [str stringByAppendingString:@"%@",@"“zs”的phone"];
     NSString *urlStr = [NSString stringWithFormat:@"https://www.baidu.com/s?%@",[NSString stringWithUTF8String:"“zs” 的 iPhone"]];
-    
-    
-    NSString * encodingString = [urlStr stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-    
-    
+    BOOL is9_0 = [[UIDevice currentDevice].systemVersion floatValue] < 9.0;
+    NSString * encodingString;
+    [urlStr stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+#ifdef is9_0
 
-    
-    
-    
-    
+   encodingString = [urlStr stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+#elseif
+    encodingString = [urlStr stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+#endif
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
     manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript", @"text/html", nil];
     
